e8af16d1e78efbbf01b90c63d9b92400a28ac024
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2022-01-16 16:25:36 -0500

    routes working
    - navbar updated
    - app.component updated to accomodate new layout
    - all pages renderig correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CustomerMainComponent } from './customer-main/customer-main.component';
@@ -20,7 +21,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'customers', component : CustomerMainComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
