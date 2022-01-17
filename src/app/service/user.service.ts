4987bb894bc720aecd385ceea3f69ffeb72a4398
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2022-01-17 14:56:06 -0500

    updated user service to post and put as well
    - user can issue put request with a user type
    - user can issue post request with a user type

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable} from 'rxjs'
 import { User } from '../interface/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +15,25 @@ export class UserService {
   // we can pass our string here so we don't have to do it all the time
   //https://jsonplaceholder.typicode.com/users
 
-  url:string = 'https://jsonplaceholder.typicode.com/users';
+  url:string = environment.apiUrl;
 
   // our getusers is going to return an array of observable users
   // we also need to remember to pass types for the user to return from
   getUsers():Observable<User[]>{
-    return this.http.get<User[]>(this.url);
+    return this.http.get<User[]>(`${this.url}/users`);
   }
 
   getUser(): Observable<User>{
-    return this.http.get<User>(`${this.url}/1`);
+    return this.http.get<User>(`${this.url}/users/1`);
+  }
+
+  createUser(user:User): Observable<User>{
+    return this.http.post<User>(`${this.url}/users`,user)
+  }
+
+  // We need only specify the return type(Observable<user>) and make sure to pass the user
+  // as the payload.
+  updateUser(user:User): Observable<User>{
+    return this.http.put<User>(`${this.url}/users/${user.id}`,user)
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
 
-import { UserService} from '../service/user.service';
+import { UserService } from '../service/user.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  private testUser: User = {
+    name: 'Leonard Nimovy',
+    username: 'spock',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  };
 
-  constructor(private userService: UserService) {
-  }
+  private testUser2: User = {
+    id: 1,
+    name: 'Leonard Nimovy',
+    username: 'spock',
+    email: 'spock@fed.up.com',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  };
 
-  onGetUsers(): void{
+  constructor(private userService: UserService) {}
+
+  onGetUsers(): void {
     this.userService.getUsers().subscribe(
       (response) => console.log(response),
       (error: any) => console.log(error),
@@ -20,8 +66,41 @@ export class UserComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
-    this.onGetUsers();
+  onGetUser(): void{
+    this.userService.getUser().subscribe(
+      (response)=> console.log(response),
+      (error: any) => console.log(error),
+      ()=> console.log('finished getting user')
+    )
   }
 
+  onCreateUser(): void{
+    this.userService.createUser(this.testUser).subscribe(
+      response => console.log(response),
+      error => console.log(error),
+      () => console.log(`finished creating users`)
+    )
+  }
+
+  onUpdateUser(): void{
+    this.userService.updateUser(this.testUser2).subscribe(
+      response => console.log(response),
+      error => console.log(error),
+      ()=> console.log(`finished updating user`)
+    )
+  }
+
+
+
+  ngOnInit(): void {
+    console.log("GETTING ALL USERS")
+    this.onGetUsers();
+    console.log("NOW GETTING USER #1")
+    this.onGetUser();
+    console.log("NOW ADDING ONE USER")
+    this.onCreateUser();
+    console.log("NOW UPDATING USER 1")
+    this.onUpdateUser();
+    this.onGetUsers();
+  }
 }
